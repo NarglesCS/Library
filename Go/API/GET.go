e68fbc9062c17e5feb8ecbedd868e7c246f44bf9
Nargles <jvschwimmer@gmail.com>
2020-05-07 18:33:25 -0400

    Add files via upload
    GET request example

---
+++ Go/API/GET.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	//http.Get request, and response handling of packet header, body, trailer
+	resp, err := http.Get("https://api.github.com/zen")
+	if err == nil {
+		head := resp.Header
+		if err == nil {
+			//Header is of type MAP with keys:
+			keys := make([]string, 0, len(head))
+			for k := range head {
+				keys = append(keys, k)
+			}
+			for i := range keys {
+				fmt.Print(keys[i], "\n")
+			}
+
+		}
+		defer resp.Body.Close()
+		//response is in the form of a packet. This gives us a memory location for the response.
+		body, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			//After ioutil, the characters are in ascii values. stirng(body) converts to a printable, readable string.
+			body := string(body)
+			fmt.Print(body, "\n")
+		}
+		//Tail of the response
+		tail := resp.Trailer
+		fmt.Print(tail)
+	}
+
+}
