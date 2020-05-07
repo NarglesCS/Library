b09c77dd019f3bf6eaec84d82d0cbc92f15fc89a
Nargles <jvschwimmer@gmail.com>
2020-05-07 18:42:31 -0400

    Add files via upload
    POST request with included JSON example

---
+++ Go/API/POST.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	//Example JSON constructor
+	requestBody, err := json.Marshal(map[string]string{
+		"Name":       "Nargles",
+		"brogrammer": "YES! NAtty lights baby",
+	})
+
+	//http.Post request, and response handling of packet header, body, trailer
+	//Note: can use NewBufferString for a quick string
+	resp, err := http.Post("https://postman-echo.com/post", "text/plain", bytes.NewBuffer(requestBody))
+	if err == nil {
+		head := resp.Header
+		if err == nil {
+			//Header is of type MAP with keys:
+			keys := make([]string, 0, len(head))
+			for k := range head {
+				keys = append(keys, k)
+			}
+			for i := range keys {
+				fmt.Print(keys[i], "\n")
+			}
+
+		}
+		defer resp.Body.Close()
+		//response is in the form of a packet. This gives us a memory location for the response.
+		body, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			//After ioutil, the characters are in ascii values. stirng(body) converts to a printable, readable string.
+			body := string(body)
+			fmt.Print(body, "\n")
+		}
+		//Tail of the response
+		tail := resp.Trailer
+		fmt.Print(tail)
+	}
+}
