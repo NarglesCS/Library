0171eb07199a34a80b9924bef22151c76d907d8c
Nargles <jvschwimmer@gmail.com>
2020-05-07 20:19:08 -0400

    Update GET.go

--- Go/API/GET.go
+++ Go/API/GET.go
@@ -14,6 +14,7 @@ func main() {
 		if err == nil {
 			//Header is of type MAP with keys:
 			keys := make([]string, 0, len(head))
+			fmt.Println("Packet header keys")
 			for k := range head {
 				keys = append(keys, k)
 			}
@@ -28,10 +29,12 @@ func main() {
 		if err == nil {
 			//After ioutil, the characters are in ascii values. stirng(body) converts to a printable, readable string.
 			body := string(body)
+			fmt.Println("Packet Body")
 			fmt.Print(body, "\n")
 		}
 		//Tail of the response
 		tail := resp.Trailer
+		fmt.Println("Packet trailer")
 		fmt.Print(tail)
 	}
 
